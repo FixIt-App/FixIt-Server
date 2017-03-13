74e41508ce82af5c3f0e5dec84145672c85cb0cf
David Calle <davidcalle9430@gmail.com>
2017-03-12 20:45:21 -0500

    worktypes admin

--- worktype/admin.py
+++ worktype/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from worktype.models import WorkType
 
-# Register your models here.
+class WorkTypeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(WorkType, WorkTypeAdmin)
