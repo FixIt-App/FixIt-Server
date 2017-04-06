6396c9e35f4dc7c84a59250d7a9eee0c435d7fb7
Alfredo Santamaria <chino.sebastian@gmail.com>
2017-04-06 10:53:42 -0500

    changing name to fixit in stop-celery

--- scripts/stop-celery.sh
+++ scripts/stop-celery.sh
@@ -1,8 +1,9 @@
 celery multi restart worker1 \
-    -A proj \
+    -A fixit \
     --logfile="../pidprofile.pid" \
     --pidfile="../celery1.log"
 
 celery multi stopwait worker1 \
      --pidfile="../celery1.log"
-
+# TODO: create pidprofile.pid file with date
+rm ../pidprofile.pid
