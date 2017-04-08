e06819fed878c4cd7c27a596408160e1e204664c
David Calle <davidcalle9430@gmail.com>
2017-04-08 16:05:00 -0500

    fixed test

--- worktype/tests.py
+++ worktype/tests.py
@@ -5,10 +5,10 @@ from worktype.models import WorkType
 class WorkTypeTestCase(TestCase):
 
     def setUp(self):
-        WorkType.objects.create(name = "Carpinteria", description = "Un trabajo muy duro", icon = "random/url.png" )
+        WorkType.objects.create(name = "Carpintería", description = "Un trabajo muy duro", icon = "random/url.png" )
 
     def test_works_are_created(self):
         """Wortypes are correctly created"""
-        worktype = WorkType.objects.get(name = "Carpinteria")
+        worktype = WorkType.objects.get(name = "Carpintería")
         
-        self.assertEqual(worktype.name, 'Carpinteria')
+        self.assertEqual(worktype.name, 'Carpintería')
