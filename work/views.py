91e904c8e8ba6d4ce11405f7c679dc1652803a0b
Alfredo Santamaria <chino.sebastian@gmail.com>
2017-04-18 17:43:21 -0500

    returning work after update

--- work/views.py
+++ work/views.py
@@ -100,7 +100,15 @@ class WorkDetail(APIView):
                 image.work = work
                 image.save()
             work.save()
-            return Response(status = status.HTTP_200_OK)
+
+            images = Image.objects.filter(work__id__exact = work.id)
+            work.images = images
+            # setting the worker to the work
+            if work.worker is not None:
+                worker = Worker.objects.get(pk = work.worker.id)
+                work.worker = worker
+            serializer = DetailWorkSerializer(work)
+            return Response(serializer.data, status = status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, pk, format = None):
