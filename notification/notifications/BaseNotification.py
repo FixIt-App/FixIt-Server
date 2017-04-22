71c1dcdf000f77a20612f3aedcff77345f986f7d
David Calle <davidcalle9430@gmail.com>
2017-04-22 18:45:41 -0500

    added notification type

--- notification/notifications/BaseNotification.py
+++ notification/notifications/BaseNotification.py
@@ -2,10 +2,11 @@
 
 class BaseNotification():
 
-    def __init__(self, notification = {}, to = None, priority = 10):
+    def __init__(self, notification = {}, to = None, priority = 10, notification_type = None):
         self.priority = priority
         self.to = to
         self.notification = notification
+        self.notification.notification_type = notification_type
         
     def export(self):
          return {
