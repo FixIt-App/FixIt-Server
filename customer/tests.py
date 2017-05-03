997e9ddb67dea3f66293d6494a871054d510bbe2
Alfredo Santamaria <chino.sebastian@gmail.com>
2017-05-03 18:57:08 -0500

    adding test edit customer email

--- customer/tests.py
+++ customer/tests.py
@@ -5,8 +5,8 @@ from rest_framework.test import APIRequestFactory, force_authenticate
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 
-from customer.views import confirm_phone
-
+from customer.views import confirm_phone, CustomerDetail
+from customer.models import Customer
 from customer.models import Confirmation
 
 class ConfirmationTests(TestCase):
@@ -45,5 +45,32 @@ class ConfirmEmail(TestCase):
     def test_fail_email_confirmation(self):
         response = self.client.get('/confirmations/thiaaa-sis-afa-ket-oke-n/')
         self.assertEqual(response.status_code, 404, 'It should not find the confirmation')
+
+class UpdateCustomer(TestCase):
+
+    fixtures = ['data']
+
+    def setUp(self):
+        self.token = Token.objects.get(user__username='david')
+        self.user = User.objects.get(pk = 1)
+        self.customer = Customer.objects.get(pk = 1)
+
+    def test_update_email(self):
+        factory = APIRequestFactory()
+
+        request_body = {
+            "email": "test@test.test",
+        }
+
+        request = factory.put('/api/customers/' + str(self.customer.id) + '/', request_body)
+        force_authenticate(request, user=self.user, token=self.token.key)
+        
+        response = CustomerDetail.as_view()(request,  self.customer.id)
+        self.assertEqual(response.status_code, 200, 'It should return 200, modified')
+
+        updatedCustomer = Customer.objects.get(pk = 1)
+        self.assertEqual(updatedCustomer.user.email, "test@test.test", 'Email should be modified')
+
+        self.assertEqual(updatedCustomer.user.username, "test@test.test", 'Username should be modified')
 
 
