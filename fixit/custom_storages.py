db4e950a29a0d3977e2098afd1efddb506dac4ea
David Calle <davidfcalle@gmail.com>
2017-05-28 20:51:56 +0000

    updated to boto3

--- fixit/custom_storages.py
+++ fixit/custom_storages.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto import S3Boto3Storage
 
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
