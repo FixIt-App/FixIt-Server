b8e5bf084132d56cd120a1582ee659a092cb5df3
Alfredo Santamaria <chino.sebastian@gmail.com>
2017-08-20 18:42:03 -0500

    deleting card without deleting TPagaCustomer

--- customer/models.py
+++ customer/models.py
@@ -26,8 +26,8 @@ class TPagaCustomer(models.Model):
     """
     customer = models.OneToOneField(Customer, on_delete = models.CASCADE, null = False)
     tpaga_id = models.CharField(max_length = 300, blank = False)
-    token = models.CharField(max_length = 300, blank = True, unique = True)
-    credit_card_id = models.CharField(max_length = 300, blank = True, unique = True)
+    token = models.CharField(max_length = 300, blank = True, null = True, unique = True)
+    credit_card_id = models.CharField(max_length = 300, blank = True, null = True, unique = True)
 
 class Address(models.Model):
     """
